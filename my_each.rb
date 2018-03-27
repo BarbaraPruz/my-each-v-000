c5fc9efaa7cc024dd1cd4a1d813329993f7ca700
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-27 18:58:29 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 def my_each (array)
-  count = 0
-  while count < array.size
-    yield array[count]
-    count += 1
+  if block_given?
+    count = 0
+    while count < array.size
+      yield array[count]
+      count += 1
+    end
   end
+  array
 end
